f4748b8c7173751534a0c14a81ea8c2c3206dee2
Leandro Furtado <le_furtado@outlook.com.br>
2024-01-09 17:34:04 -0300

    feat: 'useCarrinhoContext no componente de BotaoCarrinho'

--- src/components/BarraNavegacao/BotaoCarrinho/index.jsx
+++ src/components/BarraNavegacao/BotaoCarrinho/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 import imagemCarrinho from "/assets/cart.svg";
+import { useCarrinhoContext } from "@/hooks/useCarrinhoContext";
 
-const BotaoCarrinho = ({ className, quantidadeProdutos }) => {
+const BotaoCarrinho = ({ className }) => {
   const classesComuns = "btn ms-3";
+  const { quantidade } = useCarrinhoContext();
 
   return (
     <button
@@ -14,8 +16,8 @@ const BotaoCarrinho = ({ className, quantidadeProdutos }) => {
       aria-controls="modalCarrinhoSuspenso"
     >
       <img src={imagemCarrinho} alt="ícone de um carrinho de supermercado" />
-      {quantidadeProdutos !== 0 && (
-        <span className="badge verde-limao">{quantidadeProdutos}</span>
+      {quantidade !== 0 && (
+        <span className="badge verde-limao">{quantidade}</span>
       )}
     </button>
   );
